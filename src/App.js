a9511739d6d633baa49a1abf1ccdee637e3b6665
Edson Villarroel Ricaldes <villarroel.dev@gmail.com>
2023-04-24 18:18:49 -0400

    remove

--- src/App.js
+++ src/App.js
@@ -5,26 +5,10 @@ import About from "./about/about";
 import WorkExperience from "work-experience/workExperience";
 import MyServices from "my-services/myServices";
 import Skills from "skills/skills";
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+
 
 class App extends Component {
   render() {
-    // Your web app's Firebase configuration
-    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    // const firebaseConfig = {
-    //   apiKey: "AIzaSyDuglKpJ04Rj4bvzeSDJwq_Eg1ue7HPo_Y",
-    //   authDomain: "edson-villarroel.firebaseapp.com",
-    //   projectId: "edson-villarroel",
-    //   storageBucket: "edson-villarroel.appspot.com",
-    //   messagingSenderId: "180075244298",
-    //   appId: "1:180075244298:web:1ffe43504d4a65a3f968f3",
-    //   measurementId: "G-BYY29BYTS4",
-    // };
-
-    // Initialize Firebase
-    // const app = initializeApp(firebaseConfig);
-    // const analytics = getAnalytics(app);
     return (
       <div className="App">
         <HeaderPage></HeaderPage>
